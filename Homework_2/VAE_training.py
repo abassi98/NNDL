4213c38ad4570a5db850cc41e149929910fb02b0
lupoalberto98 <alberto.bassi.3@studenti.unipd.it>
2021-12-17 17:59:27 +0100

    Debugged VAE

--- Homework_2/VAE_training.py
+++ Homework_2/VAE_training.py
@@ -23,6 +23,7 @@ def train_epoch(net, device, dataloader, beta, loss_function, optimizer):
     # List to save batch losses
     train_div_loss = []
     train_rec_loss = []
+    
     # Iterate the dataloader
     for x_batch, _ in dataloader:
             
@@ -33,9 +34,9 @@ def train_epoch(net, device, dataloader, beta, loss_function, optimizer):
         mean, log_var, decoded_data = net(x_batch)
 
         # Compute losses
-        KLDiv_loss = nKLDivLoss(mean, log_var)
         MSE_loss = loss_function(decoded_data, x_batch)
-        loss = MSE_loss + beta*KLDiv_loss 
+        KLDiv_loss = nKLDivLoss(mean, log_var)
+        loss = MSE_loss + beta*KLDiv_loss
       
         # Backpropagation
         optimizer.zero_grad()
@@ -43,40 +44,42 @@ def train_epoch(net, device, dataloader, beta, loss_function, optimizer):
         optimizer.step()
         
         # Compute batch losses
-        KLDiv_loss = KLDiv_loss.detach().cpu().numpy()
         MSE_loss = MSE_loss.detach().cpu().numpy()
-        train_div_loss.append(KLDiv_loss)
         train_rec_loss.append(MSE_loss)
+        KLDiv_loss = KLDiv_loss.detach().cpu().numpy()
+        train_div_loss.append(KLDiv_loss)
         
     return (train_div_loss, train_rec_loss)
     
 
 ### Test function
 def val_epoch(net,  device, dataloader, loss_function):
-   # Set evaluation mode
-   net.eval()
-   # List to save evaluation losses
-   val_div_loss = []
-   val_rec_loss = []
-   with torch.no_grad():
-      for x_batch, _ in dataloader:
+    
+    # Set evaluation mode
+    net.eval()
+    # List to save evaluation losses
+    val_div_loss = []
+    val_rec_loss = []
+    with torch.no_grad():
+        for x_batch, _ in dataloader:
                 
-         # Move to device
-         x_batch = x_batch.to(device)
+            # Move to device
+            x_batch = x_batch.to(device)
 
-         # Forward pass
-         mean, log_var, decoded_data = net(x_batch)
+            # Forward pass
+            mean, log_var, decoded_data = net(x_batch)
         
-         # Compute losses
-         KLDiv_loss = nKLDivLoss(mean, log_var)
-         MSE_loss = loss_function(decoded_data, x_batch)
+            # Compute losses
+            MSE_loss = loss_function(decoded_data, x_batch)
+            KLDiv_loss = nKLDivLoss(mean, log_var)
          
-         # Compute batch losses
-         KLDiv_loss = KLDiv_loss.detach().cpu().numpy()
-         MSE_loss = MSE_loss.detach().cpu().numpy()
-         val_div_loss.append(KLDiv_loss)
-         val_rec_loss.append(MSE_loss)
+            # Compute batch losses
+            MSE_loss = MSE_loss.detach().cpu().numpy()
+            val_rec_loss.append(MSE_loss)
+            KLDiv_loss = KLDiv_loss.detach().cpu().numpy()
+            val_div_loss.append(KLDiv_loss)
+        
          
-   return (val_div_loss, val_rec_loss)
+    return (val_div_loss, val_rec_loss)
 
 
