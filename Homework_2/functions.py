7977a58fa099ce5fb8b2528073478b8dfd93e15b
lupoalberto98 <alberto.bassi.3@studenti.unipd.it>
2021-12-20 11:50:48 +0100

    Add fine tuning of autoencoder

---
+++ Homework_2/functions.py
@@ -0,0 +1,69 @@
+### This file contains useful functions for the homework
+import torch
+import torch.nn as nn
+
+
+# Test the network on test dataset to check accuracy on mnist classification
+def my_accuracy(net, device, dataloader):
+    """
+    Compute the classification accuracy of the model and confusion matrix
+    ___________
+    Parameters:
+    net = network
+    device = training device(cuda/cpu)
+    dataloader = dataloader
+  
+    ________
+    Returns:
+    mismatched = list of all mismatched examples
+    confusion_list = list whose elements are list of true labels and probabilities of the model
+    accuracy = classification accuracy
+    """
+    # Set evaluation mode
+    net.eval()
+
+    total = 0
+    correct = 0
+    mismatched = []
+    predictions = []
+    trues = []
+    
+    with torch.no_grad():
+        for  x_batch, label_batch in dataloader:
+                
+            # Move to device
+            x_batch = x_batch.to(device)
+            label_batch = label_batch.to(device)
+
+            # Forward pass
+            y_hat = net(x_batch)
+            y_hat = y_hat.squeeze()
+
+            # Apply softmax 
+            softmax = nn.Softmax(dim=0)
+            out_soft = softmax(y_hat)
+
+            # Take the prediction
+            predicted = out_soft.detach().cpu().argmax().item()
+            
+            # True value
+            true = label_batch.detach().cpu().item()
+
+            if predicted==true:
+                correct += 1
+            else:
+                mismatched.append((x_batch.detach().cpu().numpy(), predicted, true))
+                
+            # Take probabilities
+            prob = out_soft.detach().cpu().numpy()
+            
+            # Append to lists
+            predictions.append(predicted)
+            trues.append(true)
+                                  
+            total += 1
+
+    return mismatched, [trues, predictions], 100.0*correct/total
+    
+
+
--- Homework_2/training.py
+++ Homework_2/training.py
@@ -2,20 +2,22 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-### Train function
+### Normal autoencoder training functions
+# Train function
 def train_epoch(encoder, decoder, device, dataloader, loss_function, optimizer, noise = None):
     """
     Train an epoch of data
     -----------
     Parameters:
-    encoder, decoder = network
-    device = training device (cuda/cpu)
-    dataloader = dataloader of data
-    loss_function = loss function
-    optimzer = optimizer used
+    encoder, decoder: network
+    device: training device (cuda/cpu)
+    dataloader: dataloader of data
+    loss_function: loss function
+    optimzer: optimizer used
+    noise: noise to be added in denoising version
     --------
     Returns:
-    train_epoch_loss = list with all batch losses of the epoch
+    average training loss over the epoch
     """
     # Set the train mode
     encoder.train()
@@ -26,16 +28,13 @@ def train_epoch(encoder, decoder, device, dataloader, loss_function, optimizer, 
     for x_batch, _ in dataloader:
         # Move to device
         x_batch = x_batch.to(device)
-
-        # Set noise version equal to original image
-        x_batch_noised = x_batch
     
         # Add noise
         if noise:
-            x_batch_noised = noise(x_batch)
+            x_batch = noise(x_batch)
     
         # Encode data
-        encoded_data = encoder(x_batch_noised)
+        encoded_data = encoder(x_batch)
         
         # Decode data
         decoded_data = decoder(encoded_data)
@@ -51,11 +50,25 @@ def train_epoch(encoder, decoder, device, dataloader, loss_function, optimizer, 
         # Compute batch losses
         train_batch_loss = loss.detach().cpu().numpy()
         train_epoch_loss.append(train_batch_loss)
-    return train_epoch_loss
+        
+    return np.mean(train_epoch_loss)
     
 
-### Test function
+# Validation function
 def val_epoch(encoder, decoder,  device, dataloader, loss_function, noise = None):
+    """
+    Validate an epoch of data
+    -----------
+    Parameters:
+    encoder, decoder: network
+    device: training device (cuda/cpu)
+    dataloader: dataloader of data
+    loss_function: loss function
+    noise: noise to be added in denoising version
+    --------
+    Returns:
+    average validation loss over the epoch
+    """
     # Set evaluation mode
     encoder.eval()
     decoder.eval()
@@ -67,15 +80,12 @@ def val_epoch(encoder, decoder,  device, dataloader, loss_function, noise = None
             # Move to device
             x_batch = x_batch.to(device)
 
-            # Set noise version equal to original image
-            x_batch_noised = x_batch
-
             # Add noise
             if noise:
-                x_batch_noised = noise(x_batch)
+                x_batch = noise(x_batch)
 
             # Encode data
-            encoded_data = encoder(x_batch_noised)
+            encoded_data = encoder(x_batch)
  
             # Decode data
             decoded_data = decoder(encoded_data)
@@ -86,6 +96,102 @@ def val_epoch(encoder, decoder,  device, dataloader, loss_function, noise = None
             # Compute batch_loss
             val_batch_loss = loss.detach().cpu().numpy()
             val_epoch_loss.append(val_batch_loss)
-    return val_epoch_loss
+            
+    return np.mean(val_epoch_loss)
     
 
+    
+
+### Fine tuning
+# Train function
+def ft_train_epoch(encoder, device, dataloader, loss_function, optimizer, noise = None):
+    """
+    Fine tune the encoder with a supervised task
+    -----------
+    Parameters:
+    encoder: network
+    device: training device (cuda/cpu)
+    dataloader: dataloader of data
+    loss_function: loss function
+    optimzer: optimizer used
+    noise: noise to be added in denoising version
+    --------
+    Returns:
+    average training loss over the epoch
+    """
+    # Set the train mode
+    encoder.train()
+  
+    # List to save batch losses
+    train_epoch_loss = []
+    
+    # Iterate the dataloader
+    for x_batch, label_batch in dataloader:
+        # Move to device
+        x_batch = x_batch.to(device)
+        label_batch = label_batch.to(device)
+    
+        # Add noise
+        if noise:
+            x_batch = noise(x_batch)
+    
+        # Encode data
+        encoded_data = encoder(x_batch)
+
+        # Compute loss
+        loss = loss_function(encoded_data, label_batch)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # Compute batch losses
+        train_batch_loss = loss.detach().cpu().numpy()
+        train_epoch_loss.append(train_batch_loss)
+        
+    return np.mean(train_epoch_loss)
+
+
+# Validation function
+def ft_val_epoch(encoder,  device, dataloader, loss_function, noise = None):
+    """
+    Validation function for fine tuning
+    -----------
+    Parameters:
+    encoder: network
+    device: training device (cuda/cpu)
+    dataloader: dataloader of data
+    loss_function: loss function
+    noise: noise to be added in denoising version
+    --------
+    Returns:
+    average validation loss over the epoch
+    """
+    # Set evaluation mode
+    encoder.eval()
+
+    # List to save evaluation losses
+    val_epoch_loss = []
+    with torch.no_grad():
+        for x_batch, label_batch in dataloader:
+                
+            # Move to device
+            x_batch = x_batch.to(device)
+            label_batch = label_batch.to(device)
+
+            # Add noise
+            if noise:
+                x_batch = noise(x_batch)
+
+            # Encode data
+            encoded_data = encoder(x_batch)
+
+            # Compute loss
+            loss = loss_function(encoded_data, label_batch)
+
+            # Compute batch_loss
+            val_batch_loss = loss.detach().cpu().numpy()
+            val_epoch_loss.append(val_batch_loss)
+            
+    return np.mean(val_epoch_loss)
